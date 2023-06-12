319285a58cf31a7fc2869d1e54de90a30dce0cbf
Salma EL HAJRAOUI <salma.elhajraoui@jellyfish.com>
2023-06-12 15:35:07 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1 class="text-3xl">Hello Buccaneers</h1>
+  <h1 className="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+</style>
